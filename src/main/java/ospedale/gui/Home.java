ae3eeb02a0e81c3738ed6c2c36f8f4d20dab8bd1
nunzia46 <nunzia.guida@studenti.unina.it>
2026-07-11 15:49:47 +0200

    Gruppo45

--- src/main/java/ospedale/gui/Home.java
+++ src/main/java/ospedale/gui/Home.java
@@ -4,9 +4,7 @@ import javax.swing.*;
 
 public class Home {
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(() -> {
-			new ospedale.gui.LoginFrame(new ospedale.controller.Controller()).setVisible(true);
-		});
-
+		SwingUtilities.invokeLater(() ->
+				new ospedale.gui.LoginFrame(new ospedale.controller.Controller()).setVisible(true));
 	}
 }
